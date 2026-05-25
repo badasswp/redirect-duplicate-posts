aa6509c9d0797b439fcafcad96b24e9e97459369
badasswp <badasswpdev@gmail.com>
2026-05-25 00:03:46 +0000

    Build from release-1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/redirect-duplicate-posts',
+        'pretty_version' => 'dev-release-1.1.0-built',
+        'version' => 'dev-release-1.1.0-built',
+        'reference' => '570dab3137918fc016e74017ce18b2696bf80b60',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/redirect-duplicate-posts' => array(
+            'pretty_version' => 'dev-release-1.1.0-built',
+            'version' => 'dev-release-1.1.0-built',
+            'reference' => '570dab3137918fc016e74017ce18b2696bf80b60',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
