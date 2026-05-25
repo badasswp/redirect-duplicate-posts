87856e4aff33d76ecece78355b7029c22d1e2efe
badasswp <badasswpdev@gmail.com>
2026-05-25 12:52:46 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/redirect-duplicate-posts',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1872534b42a88c0c9636fbc19539a1297978df87',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/redirect-duplicate-posts' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1872534b42a88c0c9636fbc19539a1297978df87',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
